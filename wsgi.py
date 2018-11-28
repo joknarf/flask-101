463df7ef42e6d5b8b62f2df38a094642fc62398f
joknarf <joknarf@free.fr>
2018-11-28 11:17:48 +0100

    inital commit

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+# wsgi.py
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+PRODUCTS = [
+    { 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' },
+    { 'id': 3, 'name': 'Breizh.tv' },
+]
+
+@app.route('/')
+def hello():
+    return "Hello Mad World!"
+
+@app.route('/api/v1/products')
+def products():
+    return jsonify(PRODUCTS)
